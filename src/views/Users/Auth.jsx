190a74e2e06b74b2b4f49bdaaaeb53a1702ef9de
Pete Seeling <peteseeling@gmail.com>
2022-05-27 11:56:36 -0600

    auth page code written.

--- src/views/Users/Auth.jsx
+++ src/views/Users/Auth.jsx
@@ -1,3 +1,63 @@
 import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { useAuth, useCurentUser } from '../'
+import { useAuth, useCurentUser } from '../../context/UserProvider'
+
+
+export default function Authenticate(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const history = useHistory();
+    const user = useCurentUser();
+    const { login } = useAuth();
+
+   const handleSubmit = async (e) => {
+       try{
+           e.preventDefault();
+           
+           await login({ email, password });
+
+       }catch(error){
+           setError(error.message)
+       }
+   };
+
+   const handleClick = (e) => {
+       e.preventDefault();
+
+       if (!email || !password) {
+           setError('Enter valid username and Password')
+       }else{
+           setError('');
+           handleSubmit(e)
+       }
+   };
+
+   return(
+       <form onSubmit={handleSubmit}>
+           <label aria-label='email'>Email: </label>
+           <input
+            id='email'
+            type='text'
+            placeholder='email'
+            value={email}
+            onChange={({ target }) => setEmail(target.value)} />
+
+            <label aria-label='password'>Password: </label>
+            <input
+                id='password'
+                type='password'
+                placeholder='password'
+                value={password}
+                onChange={({ target }) => setPassword(target.value)} />
+
+                <button
+                    type='submit'
+                    aria-label='submit-button'
+                    onClick={handleClick}>
+                        Sign In
+                    </button>
+       </form>
+   )
+
+}
