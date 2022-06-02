bfe974efa17037bb78e506018f3fd37c4f99ac31
Pete Seeling <peteseeling@gmail.com>
2022-06-02 13:49:20 -0600

    goal accomplish button updating but not in browser

--- src/components/Goals/Goals.jsx
+++ src/components/Goals/Goals.jsx
@@ -1,6 +1,6 @@
 import { useCallback, useEffect, useState } from "react";
 import { getGoals } from "../../services/getGoals";
-import { goalAddition } from "../../services/users";
+import { goalAddition, updateGoal } from "../../services/users";
 
 export default function GoalsList() {
     const [goals, setGoals] = useState([]);
@@ -8,9 +8,6 @@ export default function GoalsList() {
 
     const callBack = useCallback(() => {
 
-
-        
-    
         async function getGoalFromLoad() {
             const fetchedGoals = await getGoals();
             setGoals(fetchedGoals);
@@ -29,7 +26,11 @@ export default function GoalsList() {
         getGoalFromLoad();
     }, [])
 
+  async function updateGoalHandler(goal_id) {
+      await updateGoal({ goal_id, goal_accomplished: true })
 
+
+}
 
     const addGoal = async (e) => {
         e.preventDefault();
@@ -63,11 +64,13 @@ export default function GoalsList() {
                   </button>
               </form>
           </div>
+         
           <div>
           {goals.map((goal) => (
               <ul key={goal.goal_id}>
                   <p>Goal Amount: {goal.goal_amount}</p>
-                  <p>Accomplished: {goal.goal_accomplished}</p>
+                  <p>Accomplished: {goal.goal_accomplished ? 'true' : 'false'}</p>
+                  <button onClick={() => updateGoalHandler(goal.goal_id)}>Goal Finished</button>
               </ul>
           ))}
           </div>
