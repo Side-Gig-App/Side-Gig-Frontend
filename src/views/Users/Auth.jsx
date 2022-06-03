459fea903b2762e047e533428fcf56ce5e043663
Clifford Maxson <Cliffmax85@gmail.com>
2022-06-03 09:31:44 -0700

    Update Auth.jsx

--- src/views/Users/Auth.jsx
+++ src/views/Users/Auth.jsx
@@ -68,7 +68,7 @@ export default function Authenticate(){
            <button
                type='submit'
                className={styles.button}
-               aria-label='submit-button'
+               aria-label='submit-button' />
                Sign In
            </button>
        </form>
