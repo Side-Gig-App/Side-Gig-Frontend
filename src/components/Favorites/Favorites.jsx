0c4ff15d07b84edf71aa27fd11a4a958749f63e5
Pete Seeling <peteseeling@gmail.com>
2022-06-02 20:37:36 -0600

    background image fixed. css

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -17,12 +17,14 @@ console.log('favs', favorites);
     return (
         <>
           {favorites.map((fav) => (
+              <div>
               <ul key={fav.gig_id}>
                   <p>{fav.gig_name}</p>
                   <p>Hourly Pay: ${fav.salary_hourly}</p>
                   <p>{fav.third_party_link}</p>
                   <button>Sign Up for {fav.gig_name}</button>
               </ul>
+              </div>
           ))}
         </>
     )
